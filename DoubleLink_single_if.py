55f4534ce67d99f3262b905e8ab8f21d6a014079
sangtastique <broux.sash@outlook.com>
2022-12-02 09:03:47 +0000

    prepare for merge

--- DoubleLink_single_if.py
+++ DoubleLink_single_if.py
@@ -4,16 +4,26 @@ from ipmininet.cli import IPCLI
 
 class DoubleLink(IPTopo):
 
+    """
+               10
+              ____
+            /      \    30
+          H1        R1 ---- H2
+            \ ____ /
+               10
+
+    """
+
     def build(self, *args, **kwargs):
 
-        params20 = {"bw":20} # bw in Mbits
-        params10 = {"bw":10}
+        params30 = {"bw":5, "delay":"20ms"} # bw in Mbits
+        params10 = {"bw":1, "delay":"20ms"}
         r1 = self.addRouter("r1")
 
         h1 = self.addHost("h1")
         h2 = self.addHost("h2")
 
-        self.addLinks((h1, r1, params10), (h1, r1, params10), (h2, r1, params20))
+        self.addLinks((h1, r1, params10), (h1, r1, params10), (r1, h2, params30))
 
         super().build(*args, **kwargs)
 
