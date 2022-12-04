0c00a4ec0370f0f58b1848b216e3f12e699f65d1
libertco <corentin@libert.be>
2022-12-04 11:08:02 +0000

    Fix simple one link size change config

--- ipmininet/run_simple_one_link_size_change.py
+++ ipmininet/run_simple_one_link_size_change.py
@@ -1,5 +1,6 @@
 from ipmininet.ipnet import IPNet
 from topologies.simple_one_link_topo import SimpOneLinkTopo
+from ipmininet.cli import IPCLI
 import time
 import os
 import sys
@@ -7,20 +8,22 @@ import sys
 net = IPNet(topo=SimpOneLinkTopo(), use_v6=False)
 try:
     net.start()
-    time.sleep(1)
 
     net["h2"].cmd("rm measurements/simple_link_different_sizes_server_measurements.txt")
     net["h1"].cmd("rm measurements/simple_link_different_sizes_client_measurements.txt")
 
+    time.sleep(1)
+
     for i in range(1, 21):
-        time.sleep(1)
-        # net["h2"].cmd("echo '["+str(i)+"MB]' >> measurements/simple_link_different_sizes_server_measurements.txt")
-        # net["h1"].cmd("echo '["+str(i)+"MB]' >> measurements/simple_link_different_sizes_client_measurements.txt")
+        net["h2"].cmd("echo '["+str(i)+"MB]' >> measurements/simple_link_different_sizes_server_measurements.txt")
+        net["h1"].cmd("echo '["+str(i)+"MB]' >> measurements/simple_link_different_sizes_client_measurements.txt")
         print("["+str(i)+"MB] Launch rapido server")
         net["h2"].cmd("./rapido -c rsa/cert.pem -k rsa/key.pem "+ net["h2"].IP() +" 2142 >> measurements/simple_link_different_sizes_server_measurements.txt &")
+        time.sleep(1) #Wait the server to start
         print("["+str(i)+"MB] Launch rapido client\n")
         net["h1"].cmd("./rapido -s "+str(i)+" -n localhost "+ net["h2"].IP() +" 2142 >> measurements/simple_link_different_sizes_client_measurements.txt")
         
+    IPCLI(net)
     
 finally:
     net.stop()
