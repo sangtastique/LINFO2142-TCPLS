706dec470ea8b61ebc011af75f2e2a9c86407681
sangtastique <broux.sash@outlook.com>
2022-12-28 12:35:23 +0100

    added delay parameter

--- ipmininet/topologies/simple_one_link_topo.py
+++ ipmininet/topologies/simple_one_link_topo.py
@@ -1,6 +1,8 @@
 from ipmininet.iptopo import IPTopo
 from ipmininet.router.config import SSHd
 from ipmininet.host.config import Named
+from ipmininet.ipnet import IPNet
+from ipmininet.cli import IPCLI
 
 class SimpOneLinkTopo(IPTopo):
     """
@@ -8,10 +10,11 @@ class SimpOneLinkTopo(IPTopo):
         H1 ---- R ---- H2
 
     """
-    def __init__( self, bw=10 ):
-        IPTopo.__init__( self )
+    def __init__( self, bw=10, delay=2, *args, **kwargs):
+        self.switch_count = 0
+        super().__init__(*args, **kwargs)
 
-        params = {"bw":bw}
+        params = {"bw":bw, "delay":"{:d}ms".format(delay)}
         r = self.addRouter("r1")
 
         h1 = self.addHost("h1")
@@ -19,16 +22,71 @@ class SimpOneLinkTopo(IPTopo):
 
         lh1r, lh2r = self.addLinks((h1, r, params), (h2, r, params))
 
+    # def addLink(self, node1, node2, delay="2ms", bw=None,
+    #             max_queue_size=None, **opts):
+    #     src_delay = None
+    #     dst_delay = None
+    #     opts1 = dict(opts)
+    #     if "params2" in opts1:
+    #         opts1.pop("params2")
+    #     try:
+    #         src_delay = opts.get("params1", {}).pop("delay")
+    #     except KeyError:
+    #         pass
+    #     opts2 = dict(opts)
+    #     if "params1" in opts2:
+    #         opts2.pop("params1")
+    #     try:
+    #         dst_delay = opts.get("params2", {}).pop("delay")
+    #     except KeyError:
+    #         pass
 
-    # def build(self, *args, **kwargs):
-    #     params = {"bw":10}
-    #     r = self.addRouter("r")
+    #     src_delay = src_delay if src_delay else delay
+    #     dst_delay = dst_delay if dst_delay else delay
 
-    #     h1 = self.addHost("h1")
-    #     h1.addDaemon(Named)
-    #     h2 = self.addHost("h2")
-    #     h2.addDaemon(Named)
+    #     # node1 -> switch
+    #     default_params1 = {"bw": bw}
+    #     default_params1.update(opts.get("params1", {}))
+    #     opts1["params1"] = default_params1
 
-    #     lh1r, lh2r = self.addLinks((h1, r, params), (h2, r, params)) # links between H1 and routers
+    #     # node2 -> switch
+    #     default_params2 = {"bw": bw}
+    #     default_params2.update(opts.get("params2", {}))
+    #     opts2["params2"] = default_params2
 
-    #     super().build(*args, **kwargs)
+    #     # switch -> node1
+    #     opts1["params2"] = {"delay": dst_delay,
+    #                         "max_queue_size": max_queue_size}
+    #     # switch -> node2
+    #     opts2["params1"] = {"delay": src_delay,
+    #                         "max_queue_size": max_queue_size}
+
+    #     # Netem queues will mess with shaping
+    #     # Therefore, we put them on an intermediary switch
+    #     self.switch_count += 1
+    #     s = "s%d" % self.switch_count
+    #     self.addSwitch(s)
+    #     return super().addLink(node1, s, **opts1), \
+    #            super().addLink(s, node2, **opts2)
+
+
+    # # def build(self, *args, **kwargs):
+    # #     params = {"bw":10}
+    # #     r = self.addRouter("r")
+
+    # #     h1 = self.addHost("h1")
+    # #     h1.addDaemon(Named)
+    # #     h2 = self.addHost("h2")
+    # #     h2.addDaemon(Named)
+
+    # #     lh1r, lh2r = self.addLinks((h1, r, params), (h2, r, params)) # links between H1 and routers
+
+    # #     super().build(*args, **kwargs)
+
+if __name__ == "__main__":
+    net = IPNet(topo=SimpOneLinkTopo(bw=10, use_v6=False))
+    try:
+        net.start()
+        IPCLI(net)
+    finally:
+        net.stop()
--- ipmininet/topologies/spoon_topo.py
+++ ipmininet/topologies/spoon_topo.py
@@ -9,27 +9,80 @@ class SpoonDouble(IPTopo):
         H1 ---- R1          H2
                  \__________/ 
 
+        bw_parallel is expected to be [bw_top_link, bw_bot_link]
+        delay_parallel is expected to be [delay_top_link, delay_bot_link]
     """
-    def __init__( self, n_parallel, bw_handle, bw_parallel ):
+    def __init__( self, n_parallel, bw_handle, bw_parallel, delay_handle=None, delay_parallel=None, *args, **kwargs):
+        
+        self.switch_count = 0
+        super().__init__(*args, **kwargs)
 
-        # Initialize topology
-        IPTopo.__init__( self )
-
-        # self.bw_handle = bw_handle
-        # self.bw_parallel = bw_parallel
         r1 = self.addRouter("r1")
 
         h1 = self.addHost("h1")
         h2 = self.addHost("h2")
 
-        # lr1r2 = self.addLink(r1, r2)
-        # lr1r2[r1].addParams(ip=("2042:12::1/64", "10.12.0.1/24"))
-        # lr1r2[r2].addParams(ip=("2042:12::2/64", "10.12.0.2/24"))
+        # Add handle link
+        if isinstance(delay_handle, str):
+            h1r1 = self.addLink(h1, r1, bw=bw_handle, delay=delay_handle)
+        else:
+            h1r1 = self.addLink(h1, r1, bw=bw_handle)
 
-        h1r1 = self.addLink(h1, r1, bw=bw_handle)
-        # h1r1[h1].addParams(ip="10.0.0.1")
-        h2r1_1 = self.addLink(h2, r1, bw=bw_parallel[0])
-        h2r1_2 = self.addLink(h2, r1, bw=bw_parallel[1])
+        # Add parallel links
+        if isinstance(delay_parallel, list):
+            h2r1_1 = self.addLink(h2, r1, bw=bw_parallel[0], delay=delay_parallel[0])
+            h2r1_2 = self.addLink(h2, r1, bw=bw_parallel[1], delay=delay_parallel[1])
+        else:
+            h2r1_1 = self.addLink(h2, r1, bw=bw_parallel[0])
+            h2r1_2 = self.addLink(h2, r1, bw=bw_parallel[1])
+
+    # We need at least 2ms of delay for accurate emulation
+    # def addLink(self, node1, node2, delay="2ms", bw=None,
+    #             max_queue_size=None, **opts):
+    #     src_delay = None
+    #     dst_delay = None
+    #     opts1 = dict(opts)
+    #     if "params2" in opts1:
+    #         opts1.pop("params2")
+    #     try:
+    #         src_delay = opts.get("params1", {}).pop("delay")
+    #     except KeyError:
+    #         pass
+    #     opts2 = dict(opts)
+    #     if "params1" in opts2:
+    #         opts2.pop("params1")
+    #     try:
+    #         dst_delay = opts.get("params2", {}).pop("delay")
+    #     except KeyError:
+    #         pass
+
+    #     src_delay = src_delay if src_delay else delay
+    #     dst_delay = dst_delay if dst_delay else delay
+
+    #     # node1 -> switch
+    #     default_params1 = {"bw": bw}
+    #     default_params1.update(opts.get("params1", {}))
+    #     opts1["params1"] = default_params1
+
+    #     # node2 -> switch
+    #     default_params2 = {"bw": bw}
+    #     default_params2.update(opts.get("params2", {}))
+    #     opts2["params2"] = default_params2
+
+    #     # switch -> node1
+    #     opts1["params2"] = {"delay": dst_delay,
+    #                         "max_queue_size": max_queue_size}
+    #     # switch -> node2
+    #     opts2["params1"] = {"delay": src_delay,
+    #                         "max_queue_size": max_queue_size}
+
+    #     # Netem queues will mess with shaping
+    #     # Therefore, we put them on an intermediary switch
+    #     self.switch_count += 1
+    #     s = "s%d" % self.switch_count
+    #     self.addSwitch(s)
+    #     return super().addLink(node1, s, **opts1), \
+    #            super().addLink(s, node2, **opts2)
 
 
     # def build(self, *args, **kwargs):
