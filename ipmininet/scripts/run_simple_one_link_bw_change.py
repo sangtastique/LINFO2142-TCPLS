e986b966f068133666cf713b90d5489bade30789
sangtastique <broux.sash@outlook.com>
2022-12-29 12:47:17 +0100

    moved python scripts

--- ipmininet/scripts/run_simple_one_link_bw_change.py
+++ ipmininet/scripts/run_simple_one_link_bw_change.py
@@ -15,6 +15,11 @@ bw_step = 2
 filename = "measurements/simple_link_different_bw.txt"
 
 original_stdout = sys.stdout 
+
+script_path = os.path.realpath(os.path.dirname(__file__))
+root_path = os.path.dirname(os.path.dirname(script_path))
+rapido_path = os.path.join(root_path, "rapido_edit", "rapido-edit")
+
 with open(filename, 'w') as f:
     sys.stdout = f 
     print("transfert size : {:.10f} MB, iterations : {:d}, max bandwidths : arange({:d}, {:d} ,{:d})".format(transfert_size, n_iter, bw_from, bw_to, bw_step))
@@ -32,11 +37,11 @@ for bwdth in np.arange(bw_from, bw_to, bw_step):
 
         h2ip = net["h2"].IP()
 
-        cmd_server = "./rapido -c rsa/cert.pem -k rsa/key.pem {:s} 2142 >> measurements/simple_link_different_bw_server_measurements.txt &".format(h2ip)
+        cmd_server = "{:s} -c rsa/cert.pem -k rsa/key.pem {:s} 2142 >> measurements/simple_link_different_bw_server_measurements.txt &".format(rapido_path, h2ip)
 
         for j in range(0, n_iter):
             time.sleep(1)
-            cmd_client = "echo '{:d} {:d} '$(./rapido -s {:d} -n localhost {:s} 2142) >> {:s}".format(bwdth, j, transfert_size, h2ip, filename)
+            cmd_client = "echo '{:d} {:d} '$({:s} -s {:d} -n localhost {:s} 2142) >> {:s}".format(bwdth, j, rapido_path, transfert_size, h2ip, filename)
             
             print("["+str(bwdth)+"MB] Launch rapido")
             net["h2"].cmd(cmd_server)
