e654b6e99cfe450a9198637e68990ae4de82358a
sangtastique <broux.sash@outlook.com>
2022-12-29 12:49:12 +0100

    moved edited rapido stuff

---
+++ rapido_edit/get_rapido.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Get absolute path
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+
+cp $SCRIPTPATH/rapido-edit.c $SCRIPTPATH/../deps/rapido/t/rapido.c
+# # Navigate to rapido folder containing rapido's repo
+cd $SCRIPTPATH/../deps/rapido
+
+# # Build rapido
+git submodule update --init
+cmake .
+make rapido
+# # Move the binary in our repo :)
+cp rapido $SCRIPTPATH/rapido-edit
